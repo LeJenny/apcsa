1a5b8d76d423d98bcd9b0071741a5e75ec791f43
Jenny Le <45854304+LeJenny@users.noreply.github.com>
2020-11-30 00:44:56 -0800

    Update Time.java

--- Time.java
+++ Time.java
@@ -14,3 +14,24 @@ class Time {
   // I added the (s) because I'm a grammar nazi lol.
   }
 }
+
+/*
+Assignment 3:  Time Conversion
+
+Prompt the user for a number in seconds.  Convert it to weeks, days, hours, minutes and seconds.
+
+Must use %.  
+Use 1 week has 7 days, 1 day has 24 hours, 1 hour has 60 minutes, and 1 minute has 60 seconds.
+Cannot pre-multiply, e.g. 1 hour has 3600 seconds.
+Cannot use loops (for, while) or conditionals (if).
+
+
+Sample Run:
+
+Please enter the number of seconds.
+>1234567
+1234567 seconds = 2 weeks 0 days 6 hours 56 minutes and 7 seconds
+
+
+Please name both filename and class name Time.
+*/
